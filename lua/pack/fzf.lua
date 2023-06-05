28c073f8b8dd857edcd453174f1e4c87a786525c
zhuifeng521 <1547614915@qq.com>
2023-06-05 21:06:19 +0800

    添加marks列表搜索

--- lua/pack/fzf.lua
+++ lua/pack/fzf.lua
@@ -41,6 +41,7 @@ function M.config()
         { 'n', '<leader>g', ':GFiles?<cr>',  { silent = true, noremap = true } },
         { 'n', '<leader>h', ':CHistory<cr>', { silent = true, noremap = true } },
         { 'n', '<leader>b', ':Buffers<cr>',  { silent = true, noremap = true } },
+        { 'n', '<leader>m', ':Marks<cr>',    { silent = true, noremap = true } },
     })
 end
 

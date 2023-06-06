dec2a26c4abcf91bda2a1aeffe0d81888d6bf0ac
zhuifeng521 <1547614915@qq.com>
2023-06-06 22:54:47 +0800

    格式化

--- lua/pack/fzf.lua
+++ lua/pack/fzf.lua
@@ -42,7 +42,7 @@ function M.config()
         { 'n', '<leader>h', ':CHistory<cr>', { silent = true, noremap = true } },
         { 'n', '<leader>b', ':Buffers<cr>',  { silent = true, noremap = true } },
         { 'n', '<leader>d', ':bd<cr>',       { silent = true, noremap = true } },
-        { 'n', '<leader>m',  ':Marks<cr>',    { silent = true, noremap = true } },
+        { 'n', '<leader>m', ':Marks<cr>',    { silent = true, noremap = true } },
     })
 end
 
